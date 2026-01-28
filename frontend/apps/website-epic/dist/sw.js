e87a7ea2bdfc8bac5fe9ff8780d5e344adbbd1cc
Alejandro <admin@aigestion.net>
2026-01-28 16:56:19 +0100

    Fix: Rebuild website-epic with correct dependencies

---
+++ frontend/apps/website-epic/dist/sw.js
@@ -0,0 +1,21 @@
+// AIGestion Service Worker
+const CACHE_NAME = 'aigestion-v1';
+const urlsToCache = [
+  '/website-epic/',
+  '/website-epic/index.html',
+  '/website-epic/manifest.json',
+  '/website-epic/icons/icon-192x192.png',
+  '/website-epic/icons/icon-512x512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
